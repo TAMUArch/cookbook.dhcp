cd2625e5f9a17c9139d0405b6f83828f8a346d25
Victoria Blessing <vblessing@arch.tamu.edu>
2015-04-30 06:13:48 -0500

    new release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jarosser06@tamu.edu'
 license          'MIT'
 description      'Installs/Configures dhcp server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.4'
