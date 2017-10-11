874e9edeb939599430655f2faec11a64438830a8
Kevin Glueck <kglueck@viz.tamu.edu>
2017-10-11 16:40:20 -0500

    new release v0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # CHANGELOG for dhcp
 
+## 0.2.1:
+
+* Added Support for Ubuntu 14.04 (pulled from the v1.0.0 unreleased tag)
+* Note: this is a special release that the College of Arch currently has
+*  in production.  Yay for uploading to the chef server with invalid
+*  versioning not found in git repo
+
 ## 0.1.1:
 
 * cleaned up alot
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'dhcp'
-maintainer       'Texas A&M'
-maintainer_email 'jarosser06@arch.tamu.edu'
+maintainer       'Texas A&M University'
+maintainer_email 'kglueck@viz.tamu.edu'
 license          'MIT'
 description      'Installs/Configures dhcp server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.1'
 supports 'ubuntu', '>= 12.04'
